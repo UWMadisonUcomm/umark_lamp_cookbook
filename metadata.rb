d31b1af16b373b164f950c4c628c47e6eeee4e78
Nick Weaver <jnweaver@wisc.edu>
2014-08-26 15:41:20 -0500

    Rename cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'uwmark_lamp_cookbook'
+name             'uwmark_lamp'
 maintainer       'Nick Weaver'
 maintainer_email 'jnweaver@wisc.edu'
 license          'All rights reserved'
 description      'Builds a Centos-based LAMP environment'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "mysql", "= 5.4.0"
 depends "apache2", "~> 2.0.0"
